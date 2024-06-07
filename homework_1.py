84966ef7b027c14b036eb35b5c87a3708b7064af
Denisbers <denis16031984@gmail.com>
2024-06-07 22:45:10 +0300

    edit commit

--- homework_1.py
+++ homework_1.py
@@ -1,1 +1,11 @@
-print('Hello world')
+number_input = int(input('enter a four-digit number:'))
+
+digit_1 = number_input // 1000
+digit_2 = (number_input // 100) % 10
+digit_3 = (number_input % 100) // 10
+digit_4 = number_input % 10
+
+print(digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
